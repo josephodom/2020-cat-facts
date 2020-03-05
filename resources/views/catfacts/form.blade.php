1e4ac932e5e18ab988d3250a813496a3bb129452
josephodom <ejosephodom@gmail.com>
2020-03-05 18:31:43 -0500

    Split template off of form; added small css file

--- resources/views/catfacts/form.blade.php
+++ resources/views/catfacts/form.blade.php
@@ -1,10 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<title>Cat Facts!</title>
-</head>
-<body>
-</body>
-</html>
+@extends ('catfacts.template')
+
+@section ('content')
+
+<h1>Cat Facts!</h1>
+
+<form method="post">
+	<fieldset>
+		<label for="fact-count">
+			How many facts would you like?
+		</label>
+		
+		<input type="number" name="fact-count" id="fact-count" placeholder="Number Of Facts" min="0" max="{{ $max ?? 0 }}">
+		
+		<button type="submit">
+			Submit
+		</button>
+	</fieldset>
+	
+	@csrf
+</form>
+
+@endsection
